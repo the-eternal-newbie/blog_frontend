e02e77592783f738277e14527ce5197b7c742e49
Carlos VP <ca.varaperez@gmail.com>
2020-10-24 17:09:03 -0500

    Adding connection to API & requesting all comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../sass/components/_comment.scss';
 
-const Comment = ({ content, editComment, id, deleteComment }) => {
+const Comment = (props) => {
     return (
-        <section class="comment">
-            <p>{content}</p>
-            <button onClick={() => editComment(id)}>Edit</button>
-            <button onClick={() => deleteComment(id)}>Delete</button>
+        <section className="comment">
+            <h3 className="author">{props.author} commented</h3>
+            {
+                (new Date().toDateString() === new Date(props.createdAt).toDateString()) ?
+                (<><h4 className="date">Today at {
+                    new Date().toLocaleString('en-US', {
+                        hour: new Date(props.createdAt).getHours(),
+                        minute: new Date(props.createdAt).getMinutes()
+                    })
+                }</h4></>) :
+                (<><h4 className="date">on {new Date(props.createdAt).toDateString()}</h4></>)
+            }
+            <p>{props.content}</p>
+            <button onClick={() => props.editComment(props.id)}>Edit</button>
+            <button onClick={() => props.deleteComment(props.id)}>Delete</button>
         </section>
     );
 };
--- src/components/CommentsBoard.js
+++ src/components/CommentsBoard.js
@@ -1,110 +1,136 @@
-import React, { useState, useRef } from 'react';
+import axios from 'axios';
+import React, { useState, createRef, Component } from 'react';
 import PostComment from './PostComment';
 import EditComment from './EditComment';
 import Comment from './Comment';
 import '../sass/components/_commentsboard.scss';
 
 let i = 0;
-const CommentsBoard = () => {
-    const [content, setContent] = useState('');
-    const [editCommentId, setEditCommentId] = useState('');
-    const [allComments, setAllComments] = useState([]);
-    const [isCreateComment, setIsCreateComment] = useState(false);
-    const [isEditComment, setIsEditComment] = useState(false);
+export default class CommentsBoard extends Component {
+    constructor() {
+        super()
+        this.getContent = createRef();
+    }
+    state = {
+        content: '',
+        editCommentId: '',
+        allComments: [],
+        isCreateComment: false,
+        isEditComment: false,
+    }
+    // const [content, setContent] = useState('');
+    // const [editCommentId, setEditCommentId] = useState('');
+    // const [allComments, setAllComments] = useState([]);
+    // const [isCreateComment, setIsCreateComment] = useState(false);
+    // const [isEditComment, setIsEditComment] = useState(false);
 
-    const getContent = useRef();
-
-    const saveContentToState = (event) => {
-        setContent(event.target.value);
+    // const getContent = useRef();
+    componentDidMount() {
+        axios.get(`http://127.0.0.1:8000/users/comments/`).then(res => {
+            const comments = res.data;
+            console.log(comments);
+            this.setState({allComments: comments})
+        });
+    }
+    saveContentToState = (event) => {
+        this.setState({content: event.target.value});
     };
 
-    const editComment = (id) => {
-        setEditCommentId(id);
-        toggleEditComment();
+    editComment = (id) => {
+        this.setState({editCommentId: id});
+        this.toggleEditComment();
     };
 
-    const toggleCreateComment = () => {
-        setIsCreateComment(!isCreateComment);
+    toggleCreateComment = () => {
+        this.setState({isCreateComment: !this.isCreateComment});
     };
 
-    const toggleEditComment = () => {
-        setIsEditComment(!isEditComment);
+    toggleEditComment = () => {
+        this.setState({isEditComment: !this.isEditComment});
     };
 
-    const saveComment = (event) => {
-        const id = i;
-        i++;
+    saveComment = (event) => {
+        const comment = {
+            userId: 1,
+            content: event.target.value
+        }
+        axios.post(`localhost:8000/users/comments`, { comment }).then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+        this.getContent.current.value = '';
+        this.toggleCreateComment();
+    };
+
+    updateComment = (event) => {
         event.preventDefault();
-        setAllComments([...allComments, { content, id }]);
-        console.log(allComments);
-        getContent.current.value = '';
-        toggleCreateComment();
-    };
-
-    const updateComment = (event) => {
-        event.preventDefault();
-        const updatedComment = allComments.map((comment) => {
-            if (comment.id === editCommentId) {
+        const updatedComment = this.state.allComments.map((comment) => {
+            if (comment.id === this.state.editCommentId) {
                 return {
                     ...comment,
-                    content: content || comment.content,
+                    content: "lala" || comment.content,
                 };
             }
             return comment;
         });
-        setAllComments(updatedComment);
-        toggleEditComment();
+        this.setState({allComments: updatedComment});
+        this.toggleEditComment();
     };
 
-    const deleteComment = (id) => {
-        const modifiedComment = allComments.filter(
+     deleteComment = (id) => {
+        const modifiedComment = this.state.allComments.filter(
             (comment) => comment.id !== id
         );
-        setAllComments(modifiedComment);
+        this.setState({allComments: modifiedComment});
     };
 
-    return (
-        <div className="comments_board">
-            <h2>All comments</h2>
-            {!allComments.length ? (
-                // <div>
-                <h3>There are not comments yet!</h3>
-                // </div>
-            ) : (
-                allComments.map((comment) => {
-                    if (isEditComment && comment.id === editCommentId) {
-                        return (
-                            <EditComment
-                                content={comment.content}
-                                updateComment={updateComment}
-                                saveContentToState={saveContentToState}
-                            />
-                        );
-                    } else {
-                        return (
-                            <Comment
-                                id={comment.id}
-                                key={comment.id}
-                                content={comment.content}
-                                editComment={editComment}
-                                deleteComment={deleteComment}
-                            />
-                        );
-                    }
-                })
-            )}
-            {isCreateComment ? (
-                <PostComment
-                    saveContentToState={saveContentToState}
-                    getContent={getContent}
-                    saveComment={saveComment}
-                />
-            ) : (
-                <div></div>
-            )}
-            <br />
-            <button className="post" onClick={toggleCreateComment}>Post a comment</button>
-        </div>
-    );
+    render() {
+        return (
+            <div className="comments_board">
+                <h2>All comments</h2>
+                {!this.state.allComments.length ? (
+                    // <div>
+                    <h3 className="empty">There are not comments yet!</h3>
+                    // </div>
+                ) : (
+                    this.state.allComments.map((comment) => {
+                        if (this.state.isEditComment && comment.id === this.state.editCommentId) {
+                            return (
+                                <EditComment
+                                    createdAt={comment.createdAt}
+                                    author={comment.user.username}
+                                    content={comment.content}
+                                    updateComment={this.updateComment}
+                                    saveContentToState={this.saveContentToState}
+                                />
+                            );
+                        } else {
+                            return (
+                                <Comment
+                                    id={comment.id}
+                                    key={comment.id}
+                                    createdAt={comment.createdAt}
+                                    author={comment.user.username}
+                                    content={comment.content}
+                                    editComment={this.editComment}
+                                    deleteComment={this.deleteComment}
+                                />
+                            );
+                        }
+                    })
+                )}
+                {this.state.isCreateComment ? (
+                    <PostComment
+                        saveContentToState={this.saveContentToState}
+                        getContent={this.getContent}
+                        saveComment={this.saveComment}
+                    />
+                ) : (
+                    <div></div>
+                )}
+                <br />
+                <button className="post" onClick={this.toggleCreateComment}>Post a comment</button>
+            </div>
+        );
+    }
 };
-export default CommentsBoard;
